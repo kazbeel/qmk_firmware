8ff3b121f758ea6cc3443604b73c5f44e194d89d
dvalladolid <diego@zennio.com>
2023-01-12 22:03:07 +0100

    Add Kazbeel's BM40 keymap

---
+++ keyboards/kprepublic/bm40hsrgb/keymaps/kazbeel/rules.mk
@@ -0,0 +1,8 @@
+SRC += babel/babel.c babel/babel_linux.c babel/babel_windows.c babel/babel_mac.c
+
+BOOTMAGIC_ENABLE = yes
+MOUSEKEY_ENABLE = no
+EXTRAKEY_ENABLE = yes
+
+CONSOLE_ENABLE = yes
+DEFERRED_EXEC_ENABLE = yes
